f68636b2992ce14b1853368b1bec6ece7ba1fb70
damart24 <82331644+damart24@users.noreply.github.com>
2021-12-17 16:10:26 +0100

    Basement encapsulado

--- src/basement.js
+++ src/basement.js
@@ -19,22 +19,11 @@ export default class Basement extends Phaser.GameObjects.Sprite {
     constructor(scene, player, x, y, scaleX, scaleY, i, camera, sprite, otherBasement) {
         super(scene, x, y, sprite, i);
         this.scene.add.existing(this);
-        //Escala la imagen
-        this.displayWidth = scaleX;
-        this.displayHeight= scaleY;
 
-        //Se añaden físicas que no sea visible y guardamos la k para que sea interactuable
-        this.scene.physics.add.existing(this, true);
-        this.k = this.scene.input.keyboard.addKey('K');
-
-        // Unimos el segundo Basement al primero, el primero cuando llegue a este punto no recibirá Basement,
-        // Por lo que será undefined
-        this.otherBasement = otherBasement;
-        if(this.otherBasement !== undefined){
-            this.otherBasement.otherBasement = this;
-        }
-
-        this.offsetToTeleportY = -150;
+        this.scaleBasement(scaleX, scaleY);
+        this.assignOtherBasement(otherBasement);
+        this.assignInputAndVariables();
+        this.cameraVariablesBasement();
         
         this.ladderSound = this.scene.sound.add("ladderSound");
         //Al pulsar la k pasa de arriba abajo o viceversa, encaja la cámara y hace un ruido
@@ -42,9 +31,6 @@ export default class Basement extends Phaser.GameObjects.Sprite {
             if (Phaser.Input.Keyboard.JustDown(this.k)) {
                 this.ladderSound.play();
                 let newPosition = this.otherBasement.getBottomCenter();
-                this.cameraXScroll = 0;
-                this.cameraYScrollForGoingDown = 250;
-                this.cameraYScrollForGoingUp = 255;
                 player.tp(newPosition.x, newPosition.y + this.offsetToTeleportY);
                 if (i){
                     camera.setScroll(this.cameraXScroll,this.cameraYScrollForGoingDown);
@@ -54,5 +40,33 @@ export default class Basement extends Phaser.GameObjects.Sprite {
                 } 
             }
         });
+    }
+
+    //Escala la imagen
+    scaleBasement(scaleX, scaleY) {
+        this.displayWidth = scaleX;
+        this.displayHeight = scaleY;
+    }
+    
+    // Unimos el segundo Basement al primero, el primero cuando llegue a este punto no recibirá Basement,
+    // Por lo que será undefined
+    assignOtherBasement(otherBasement) {
+        this.otherBasement = otherBasement;
+        if (this.otherBasement !== undefined) {
+            this.otherBasement.otherBasement = this;
+        }
+    }
+ 
+    //Se añaden físicas que no sea visible y guardamos la k para que sea interactuable
+    assignInputAndVariables() {
+        this.scene.physics.add.existing(this, true);
+        this.k = this.scene.input.keyboard.addKey('K');
+        this.offsetToTeleportY = -150;
+    }
+
+    cameraVariablesBasement() {
+        this.cameraXScroll = 0;
+        this.cameraYScrollForGoingDown = 250;
+        this.cameraYScrollForGoingUp = 255;
     }
 }
