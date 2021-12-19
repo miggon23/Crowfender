86f52e9b34d8c0e000084a40ba82836ccc73cbe7
damart24 <82331644+damart24@users.noreply.github.com>
2021-12-19 17:47:47 +0100

    Cambiado menú inicio

--- src/menu.js
+++ src/menu.js
@@ -27,28 +27,28 @@ import Button from "./button.js";
         camera.x = 0;
         camera.y = 0;
 
-        this.optionsButton = new Button(this, 460, 300, 'OPCIONES', { fill: '#0f0' });
-        this.add.existing(this.optionsButton);
+        // this.optionsButton = new Button(this, 460, 300, 'OPCIONES', { fill: '#fff' });
+        // this.add.existing(this.optionsButton);
 
-        this.playButton = new Button(this, 470, 250, 'JUGAR', { fill: '#0f0' });
+        this.playButton = new Button(this, 470, 470, 40, 60,  'Play', { fill: '#fff' });
         this.add.existing(this.playButton);
         this.playButton.on('pointerup', () => {
             this.buttonSound.play();
-            this.easyButton = new Button(this, 410, 270, 'Facil', { fill: '#0f0' });
+            this.easyButton = new Button(this, 400, 510, 40, 60,  'Easy', { fill: '#fff' });
             this.add.existing(this.easyButton);
             this.easyButton.on('pointerup', () => {
-                this.buttonSound.play();
+                this.buttonSound.play()
                 this.menuMusic.stop();
                 this.scene.start('level', { multiplier: 1, timeToWin: 2 });
             });
-            this.normalButton = new Button(this, 470, 270, 'Normal', { fill: '#0f0' });
+            this.normalButton = new Button(this, 475, 510, 40, 60,  'Medium', { fill: '#fff' });
             this.add.existing(this.normalButton);
             this.normalButton.on('pointerup', () => {
                 this.buttonSound.play();
                 this.menuMusic.stop();
                 this.scene.start('level', { multiplier: 2, timeToWin: 3 });
             });
-            this.difficultButton = new Button(this, 540, 270, 'Dificil', { fill: '#0f0' });
+            this.difficultButton = new Button(this, 550, 510, 40, 60,  'Hard', { fill: '#fff' });
             this.add.existing(this.difficultButton);
             this.difficultButton.on('pointerup', () => {
                 this.buttonSound.play();
