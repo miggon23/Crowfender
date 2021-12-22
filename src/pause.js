9fddb647104c7ffb9917714e4fd465fa8c72c8ef
theligero <iligero@ucm.es>
2021-12-22 12:52:28 +0100

    Código comentado del pause

--- src/pause.js
+++ src/pause.js
@@ -25,23 +25,25 @@ import Button from "./button.js";
    * @override
    */
   create() {
-
+    // Texto que pone PAUSA
     this.texto = this.add.text(450, 300, 'PAUSA', { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' });
     this.p = this.input.keyboard.addKey('P');
 
+    // Botón para volver al inicio
     this.menuButton = new Button(this, 400, 330, 25, 150, 'Volver al menú', { fill: '#fff' });
     this.add.existing(this.menuButton);
-    this.menuButton.on('pointerup', () => {
+    this.menuButton.on('pointerup', () => { // Si se pulsa el botón
       this.scene.stop('pause');
       this.scene.stop('level');
       this.game.sound.stopAll();
       this.scene.start('menu');
-    });
+    }); // Se borra todo, se paran sonidos y se vuelve al menú
 
+    // Si se pulsa la p de nuevo
     this.p.on('down', function () {
       this.texto.destroy();
       this.menuButton.destroy();
       this.scene.resume('level');
-    }, this);
+    }, this); // Se vuelve al juego
   }
 }
